9155c8b6a4e0af5f0a93df5b6a1a39a23b5f9af8
jacksonohallo <jacksonohallo@gmail.com>
2022-08-25 20:48:26 +0300

    commit tree bd3f6b7e3f518cbc55cbaae0f56287334bc926a3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
   # write code here
+  attr_reader :length, :width, :height
+
+  def initialize(length,width,height)
+    @length = length
+    @width = width
+    @height = height
+  end
+
+  def kind
+    if length == width && width == height
+      return :equilateral
+    elsif length == width || width == height|| length == height
+      return :equilateral
+    else
+      return :scalene
+    end
+  end
+
+
+  def validate_triangle
+    real_triangle =  [(length + width > height), (length + height > width), (width + height > length)]
+      [length, width, height].each do |side|
+        real_triangle << false if side <= 0 
+      raise TriangleError if real_triangle.include?(false)
+      end
+  end
+      
+  class TriangleError < StandardError
+    def message
+      "Triangle inequality failed"
+    end
+  end
+
 end
